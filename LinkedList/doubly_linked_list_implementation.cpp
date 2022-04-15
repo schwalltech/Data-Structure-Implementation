2848fc0bdbca8973fe1f1ce41b05252c9b582bc7
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-15 22:36:32 +0530

    Create doubly_linked_list_implementation.cpp

---
+++ LinkedList/doubly_linked_list_implementation.cpp
@@ -0,0 +1,330 @@
+#include<stdlib.h>
+#include<iostream>
+
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+    node *prev;
+    
+};
+void createList(node **root,int no)
+{   
+    node *temp,*r;
+    if(*root==NULL)
+    {
+        *root=new node;
+        (*root)->data=no;
+        (*root)->next=NULL;
+        (*root)->prev=NULL;
+
+    }
+    else{
+
+        temp=new node;
+        temp->data=no;
+        temp->next=NULL;
+        r=*root;
+        while(r->next!=NULL)
+        {
+            r=r->next;
+        }
+        r->next=temp;
+        temp->prev=r;
+    }
+}
+void addFirst(node **root,int no)
+{
+    node *temp;
+    if(*root==NULL)
+    {
+        cout<<"Invalid operations\nFirst you have to create LinkedList then you can add first node in that\n";
+    }
+    else{
+
+        temp=new node;
+        temp->data=no;
+        temp->next=*root;
+        (*root)->prev=temp;
+        *root=temp;
+    }
+}
+void addLast(node *root,int no)
+{
+    node *temp;
+    if(root==NULL)
+    {
+        cout<<"Invalid Operation\nLinked List is empty you cannot add last node first you have to create list then you can add last node\n";
+    }
+    else{
+
+        temp=new node;
+        temp->data=no;
+        temp->next=NULL;
+
+        while(root->next!=NULL)
+        {
+            root=root->next;
+        }
+        root->next=temp;
+        temp->prev=root;
+    }
+}
+int count(node *root)
+{
+    int counter=0;
+    while(root!=NULL)
+    {
+        counter++;
+        root=root->next;
+        
+    }
+    return counter;
+}
+void insertAtPossition(node *root,int no,int pos)
+{
+    int len=count(root);
+    if(pos>0 && pos<=len)
+    {
+        node *temp,*s;
+        temp=new node;
+        temp->data=no;
+        temp->next=NULL;
+        for(int i=1;i<pos;i++)
+        {
+            root=root->next;
+        }
+        s=root->next;
+        temp->next=s;
+        temp->prev=root;
+        root->next=temp;
+        if(s!=NULL)
+        {
+            s->prev=temp;
+        }
+
+    }
+    else{
+
+        cout<<"Out of range\n";
+    }
+} 
+void display(node *root)
+{
+    if(root==NULL)
+    {
+        cout<<"Linked List is empty\n";
+    }
+    else{
+
+        while(root!=NULL)
+        {
+            cout<<root->data<<"->";
+            root=root->next;
+        }
+        cout<<"\n";
+    }
+}
+void delFirst(node **root)
+{
+    node *temp;
+    if(*root==NULL)
+    {
+        cout<<"Linked List is empty\n";
+    }
+    else if((*root)->next==NULL){
+        temp=*root;
+        *root=(*root)->next;
+        delete temp;
+    }
+    else{
+        temp=*root;
+        *root=(*root)->next;
+        (*root)->prev=NULL;
+        delete temp;
+    }
+}
+void delLast(node **root)
+{
+    node *temp,*r;
+    if(*root==NULL)
+    {
+        cout<<"LinkedList is empty";
+    }
+    else if((*root)->next==NULL)
+    {
+        temp=*root;
+        (*root)=(*root)->next;
+        delete temp;
+    }
+    else{
+
+        temp=*root;
+
+        while(temp->next!=NULL)
+        {
+           r=temp;
+           temp=temp->next;
+        }
+        r->next=NULL;
+        delete temp;
+        
+    }
+}
+void delByPossition(node **root,int pos)
+{
+    node *t,*r,*s;
+    if(*root==NULL)
+    {
+        cout<<"Linked List is empty\n";
+    }
+    else if(pos==1){
+        t=*root;
+        *root=(*root)->next;
+        delete t;
+    }
+    else{
+
+        int len=count(*root);
+        if(pos>0 && pos<=len)
+        {
+            t=*root;
+            for(int i=1;i<pos;i++)
+            {
+                r=t;
+                t=t->next;
+            }
+            s=t->next;
+            r->next=s;
+            if(s!=NULL)
+            {
+                s->prev=r;
+            }
+            delete t;
+        }
+        else{
+            cout<<"Out of range\n";
+        }
+    }
+}
+void reverse(node **root)
+{
+    node *r,*t,*s;
+    r=NULL;
+    t=*root;
+    while (t!=NULL)
+    {
+        s=r;
+        r=t;
+        t=t->next;
+        r->next=s;
+        if(s!=NULL)
+        {
+
+        }
+    }
+    r->prev=NULL;
+    *root=r;
+    
+}
+void sort(node *root)
+{
+    node *p,*q;
+    int temp;
+    p=root;
+
+    while(p->next!=NULL)
+    {
+        q=p->next;
+        while (q!=NULL)
+        {
+            if(p->data>q->data)
+            {
+                temp=p->data;
+                p->data=q->data;
+                q->data=temp;
+            }
+            q=q->next;
+        }
+        p=p->next;
+    }
+}
+int main()
+{   
+    node *head=NULL;
+    int ch,no,pos;
+    while(true)
+    {
+        
+        cout<<"Case 1: Create Linked List\n";
+        cout<<"Case 2: Add First Node\n";
+        cout<<"Case 3: Add Last Node\n";
+        cout<<"Case 4: Add Node At Posiition\n";
+        cout<<"Case 5: Display Linked List\n";
+        cout<<"Case 6: Delete first node\n";
+        cout<<"Case 7: Delete last node\n";
+        cout<<"Case 8: Delete By Possition\n";
+        cout<<"Case 9: Reverse Linked List\n";
+        cout<<"Case 10: Sort Linked List\n";
+        cout<<"Case 11: Exit\n";
+        cout<<"Enter your choice\n";
+        cin>>ch;
+
+        switch (ch)
+        {
+            case 1:
+                cout<<"enter no\n";
+                cin>>no;
+                createList(&head,no);
+                break;
+            case 2:
+                cout<<"enter no\n";
+                cin>>no;
+                addFirst(&head,no);
+                break;
+            case 3:
+                cout<<"enter no\n";
+                cin>>no;
+                addLast(head,no);
+                break;        
+            case 4:
+                cout<<"enter no\n";
+                cin>>no;
+                cout<<"enter pos to add no\n";
+                cin>>pos;
+                insertAtPossition(head,no,pos);
+                break;
+            case 5:
+                display(head);
+                break;    
+
+            case 6:
+                delFirst(&head);
+                break;    
+            case 7:
+                delLast(&head);
+                break;    
+            case 8:
+                cout<<"enter pos\n";
+                cin>>no;
+                delByPossition(&head,no);
+                break;    
+            case 9:
+                reverse(&head);
+                break;    
+            case 10:
+               sort(head);
+                break;    
+
+            case 11:
+                exit(0);
+                break;    
+            default:
+                cout<<"Please enter valid choice\n";
+                break;
+            }
+    }
+
+    return 0;
+}
