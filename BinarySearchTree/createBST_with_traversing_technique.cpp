97ccd6fd23f4a95eb297de6123ba8f3d3ef158d2
Shaukat Makandar <39257815+shaukatmakandar786@users.noreply.github.com>
2022-04-16 01:32:13 +0530

    Create createBST_with_traversing_technique.cpp

---
+++ BinarySearchTree/createBST_with_traversing_technique.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+
+using namespace std;
+struct node
+{
+    node *left;
+    int data;
+    node *right;
+};
+node *root;
+void createTree(int no)
+{
+    node *t,*r;
+    if(root==NULL)
+    {
+       root=new node;
+       root->data=no;
+       root->left=NULL;
+       root->right=NULL;
+    }
+    else{
+
+        t=new node;
+        t->data=no;
+        t->left=NULL;
+        t->right=NULL;
+
+        r=root;
+        while(r!=NULL)
+        {
+            if(no>r->data)
+            {
+                if(r->right==NULL)
+                {
+                    r->right=t;
+                    break;
+                }
+                else{
+
+                    r=r->right;
+                }
+            }
+            else{
+
+                if(r->left==NULL)
+                {
+                    r->left=t;
+                    break;
+                }
+                else{
+
+                    r=r->left;
+                }
+            }
+        }
+
+    }
+}
+void inorder(node *t)
+{
+    if(t!=NULL)
+    {
+        inorder(t->left);
+        cout<<t->data<<" ";
+        inorder(t->right);
+    }
+}
+void preorder(node *t)
+{
+    if(t!=NULL)
+    {
+        cout<<t->data<<" ";
+        preorder(t->left);
+        preorder(t->right);
+
+    }
+}
+void postorder(node *t)
+{
+    if(t!=NULL)
+    {
+        postorder(t->left);
+        postorder(t->right);
+        cout<<t->data<<" ";
+    }
+}
+int main()
+{
+
+    createTree(10);
+    createTree(30);
+    createTree(25);
+    createTree(7);
+    createTree(8);
+    createTree(4);
+    createTree(14);
+    createTree(6);
+    
+    inorder(root);
+    cout<<endl<<endl<<endl;
+
+    preorder(root);
+    cout<<endl<<endl<<endl;
+
+    postorder(root);
+    cout<<endl<<endl<<endl;
+
+}
+
+4 6 7 8 10 14 25 30 
+
+
+10 7 4 6 8 30 25 14 
+
+
+6 4 8 7 14 25 30 10 
